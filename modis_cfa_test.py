e1b783dc01878e58407150e593e4e0abbf3bd6af
Sukhada Deshpande <sdeshpa1@umbc.edu>
2020-06-19 17:32:37 -0400

    Changes in CFA Test

--- modis_cfa_test.py
+++ modis_cfa_test.py
@@ -7,8 +7,6 @@ if __name__ == '__main__':
     M06_dir_path = 'X'
     M03_dir_path = 'Y'
     M03_dir, M06_dir = getInputDirectories(M06_dir_path,M03_dir_path)
-    print(M06_dir)
-    print(M03_dir)
     # M03_files = sorted(glob.glob(M03_dir + "MYD03.A2008*"))
     # M06_files = sorted(glob.glob(M06_dir + "MYD06_L2.A2008*"))
     # t0 = time.time()
