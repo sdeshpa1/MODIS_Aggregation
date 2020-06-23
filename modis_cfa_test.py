9c999aa5869895fe03ea66c4c0446c29eb16de35
Sukhada Deshpande <sdeshpa1@umbc.edu>
2020-06-23 11:44:56 -0400

    modis_cfa_test file updated

--- modis_cfa_test.py
+++ modis_cfa_test.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     M06_dir_path = 'X'
     M03_dir_path = 'Y'
     M03_dir, M06_dir = getInputDirectories(M06_dir_path,M03_dir_path)
-    # M03_files = sorted(glob.glob(M03_dir + "MYD03.A2008*"))
-    # M06_files = sorted(glob.glob(M06_dir + "MYD06_L2.A2008*"))
+    M03_files = sorted(glob.glob(M03_dir + "MYD03.A2008*"))
+    M06_files = sorted(glob.glob(M06_dir + "MYD06_L2.A2008*"))
     # t0 = time.time()
     # # calculate cloud fraction
     # cf = calculateCloudFraction(M03_files, M06_files)
