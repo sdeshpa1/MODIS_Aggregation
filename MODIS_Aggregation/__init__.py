50827c5935be282505f0d222ee6c4445ceccd22b
Sukhada Deshpande <sdeshpa1@umbc.edu>
2020-07-19 17:24:03 -0400

    test_MODIS.py file changed

--- MODIS_Aggregation/__init__.py
+++ MODIS_Aggregation/__init__.py
@@ -8,7 +8,6 @@ __version__ = '0.1.0'
 from .cloud_fraction_aggregate import *
 from .baseline_series import *
 
-
 # if somebody does "from Sample import *", this is what they will
 # be able to access:
 __all__ = [
