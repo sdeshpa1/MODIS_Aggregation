fe89b716a35932c82836b6f4b98d8bb8494053ed
Sukhada Deshpande <sdeshpa1@umbc.edu>
2020-08-04 16:42:50 -0400

    updated travis.yml

--- MODIS_Aggregation/__init__.py
+++ MODIS_Aggregation/__init__.py
@@ -7,6 +7,7 @@ __version__ = '0.1.0'
 
 from .cloud_fraction_aggregate import *
 from .baseline_series import *
+from .checkaddition import *
 
 # if somebody does "from Sample import *", this is what they will
 # be able to access:
@@ -21,4 +22,5 @@ __all__ = [
     ,'cal_stats'
     ,'run_modis_aggre'
     ,'addGridEntry'
+    ,'addition'
 ]
